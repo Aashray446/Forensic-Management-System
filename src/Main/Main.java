b7cfe17a515cec67fca5e09e30ccf7884cd3776c
Aashray <aashray446@gmail.cocmm>
2021-12-23 13:21:05 +0530

    Please add .src folder only to the github

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,15 +1,17 @@
 package Main;
 import Main_objects.*;
+import Main_objects.Evidence.Evidence;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import Helper_class.handle_dbms;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String a = "asdasd asdasda sdea sd";
-        handle_dbms dbms = new handle_dbms();
-        ArrayList ad = dbms.read("user.csv");
-        
-        
+        String a = "asdasd,asdasda,sdea,sd";
     }
+   
 }
